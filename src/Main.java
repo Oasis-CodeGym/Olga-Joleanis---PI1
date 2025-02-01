e0bdfdae1b013be0feced2a6262c11f1aacf796b
Olga Joleanis <olgajoleanisb@gmail.com>
2025-01-31 23:17:32 -0500

    se cambia el comentario de la clase principal

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 /**
- * Clase principal del programa, que ejecuta el menú y controla la lógica del programa.
+ * Clase Main, es la clase principal del programa, que ejecuta el menú y controla la lógica del programa.
  */
+
 public class Main {
     private static Menu menu = Menu.getInstance();
 
     public static void main(String[] args) {
         menu.showMenu();
+
     }
 }
